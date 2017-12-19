b2aecc6e30140d6a98bd023e40c1a8cfcbbd2bf8
cereceres <jesus.edelcereceres@gmail.com>
2017-12-18 19:59:15 -0600

    the test are ready

---
+++ index.js
@@ -0,0 +1,30 @@
+
+module.exports = (graph = [], node = 0) => {
+    if (!Array.isArray(graph)) return [];
+    const marked = new Set();
+    const nodes = Math.sqrt(graph.length);
+    const path = new Array(nodes).fill(Infinity);
+    let actualNode = node;
+    let actualDistance = 0;
+    marked.add(actualNode);
+    path[node] = actualDistance;
+    while(marked.size < nodes) {
+        for (let i = 0; i < nodes; i++) {
+            if (marked.has(i)) continue;
+            const distance = actualDistance + graph[actualNode + i];
+            if (path[i] > distance) path[i] = distance;
+        }
+        marked.add(actualNode);
+        let minPath = Infinity;
+        let nodeWithMinDistance = undefined;
+        path.forEach((distance, index) => {
+            if (marked.has(index) || distance >= minPath) return;
+            nodeWithMinDistance = nodeWithMinDistance || index;
+            minPath = distance;
+            nodeWithMinDistance = index;
+        });
+        actualNode = nodeWithMinDistance;
+        actualDistance = minPath;
+    }
+    return Array.from(marked);
+};
---
+++ test/index.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const dijkstra = require('../index');
+describe('test to algorithm', () => {
+    it('should return a array', () => {
+        const index = Math.round(Math.random() * 4);
+        const res = dijkstra([
+            Infinity, 2, Infinity, 4,
+            Infinity, 6, 5, 4,
+            7, 2, Infinity, 4,
+            7, 5, 3, Infinity
+        ], index);
+        assert(res[index] === 0);
+    });
+
+    it('should return a array', () => {
+        const index = Math.round(Math.random() * 4);
+        const res = dijkstra([
+            Infinity, 1, Infinity, 1,
+            Infinity, 1, 1, 1,
+            1, 1, Infinity, 1,
+            1, 1, 1, Infinity
+        ], index);
+        assert(res[index] === 0);
+    });
+});
